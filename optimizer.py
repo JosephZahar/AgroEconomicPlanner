121fcbd74369b8d27756d965efaf513a21037234
ZPygo <70657426+ZPygo@users.noreply.github.com>
2023-11-15 15:14:13 -0500

    updated MIDP

--- optimizer.py
+++ optimizer.py
@@ -7,41 +7,50 @@ warnings.filterwarnings("ignore")
 # Continuous Variables
 rice_types = {
     'super': {'buy': ['dec', 'jan', 'feb', 'mar'],
-              'pay': ['mar', 'apr', 'may', 'june'],
-              'sell': ['feb', 'mar', 'apr', 'may', 'june', 'jul', 'aug', 'sept', 'oct', 'nov', 'dec_2', 'jan_2', 'feb_2']},
+              'pay': ['mar', 'apr', 'may', 'june', 'jul', 'aug'],
+              'sell': ['feb', 'mar', 'apr', 'may', 'june', 'jul', 'aug', 'sept', 'oct', 'nov', 'dec_2', 'jan_2', 'feb_2', 'mar_2', 'apr_2']},
     'kanait': {'buy': ['dec', 'jan', 'feb', 'mar', 'april', 'may'],
-               'pay': ['mar', 'apr', 'may', 'june', 'jul', 'aug'],
-               'sell': ['mar', 'apr', 'may', 'june', 'jul', 'aug', 'sept', 'oct', 'nov', 'dec_2', 'jan_2', 'feb_2']},
-    '386': {'buy': ['sept', 'oct', 'nov'],
-            'pay': ['dec_2', 'jan_2', 'feb_2'],
-            'sell': ['dec_2', 'jan_2', 'feb_2']}
+               'pay': ['mar', 'apr', 'may', 'june', 'jul', 'aug', 'sept', 'oct'],
+               'sell': ['mar', 'apr', 'may', 'june', 'jul', 'aug', 'sept', 'oct', 'nov', 'dec_2', 'jan_2', 'feb_2', 'mar_2', 'apr_2']},
+    'cheap': {'buy': ['sept', 'oct', 'nov'],
+            'pay': ['dec_2', 'jan_2', 'feb_2', 'mar_2', 'apr_2'],
+            'sell': ['dec_2', 'jan_2', 'feb_2', 'mar_2', 'apr_2']}
 }
 
 deposit_perc = 0.15
 init_investment = 300000
+sale_profit = [1.13]*len(rice_types['super']['buy']) + \
+              [1.18]*len(rice_types['kanait']['buy']) + \
+              [1.12]*len(rice_types['cheap']['buy'])
 
 rice_vars_dict = {}
-for rice, operations in rice_types.items():
+for rices, operations in rice_types.items():
     for i in range(1, len(operations['buy'])+1):
-        rice_vars_dict[f"{rice}_{i}"] = {}
-        rice_vars_dict[f"price_{rice}_{i}"] = LpVariable(f"price_{rice}_{i}", lowBound=0, cat='Integer')
+        rice_vars_dict[f"{rices}_{i}"] = {}
+        rice_vars_dict[f"price_{rices}_{i}"] = LpVariable(f"price_{rices}_{i}", lowBound=0, cat='Integer')
         for operation, months in operations.items():
-            rice_vars_dict[f"{rice}_{i}"][operation] = LpVariable.dicts(f"{rice}_{i}_{operation}", months, lowBound=0, cat='Integer')
             if operation == 'buy':
-                for month in months:
-                    rice_vars_dict[f"is_buy_{month}_{rice}_{i}?"] = LpVariable(f"price_{rice}_{i}", lowBound=0, upBound=1, cat='Integer')
+                rice_vars_dict[f"{rices}_{i}"][operation] = {months[i-1]: LpVariable(f"{rices}_{i}_{operation}_{months[i-1]}", upBound=0, cat='Integer')}
+            elif operation == 'pay':
+                rice_vars_dict[f"{rices}_{i}"][operation] = LpVariable.dicts(f"{rices}_{i}_{operation}", months[i-1:], upBound=0, cat='Integer')
+            else:
+                rice_vars_dict[f"{rices}_{i}"][operation] = LpVariable.dicts(f"{rices}_{i}_{operation}", months[i-1:], lowBound=0, cat='Integer')
 
-months = ['dec', 'jan', 'feb', 'mar', 'april', 'may', 'june', 'jul', 'aug', 'sept', 'oct', 'nov', 'dec_2', 'jan_2', 'feb_2']
+months = ['dec', 'jan', 'feb', 'mar', 'apr', 'may', 'june', 'jul', 'aug', 'sept', 'oct', 'nov', 'dec_2', 'jan_2', 'feb_2', 'mar_2', 'apr_2']
 bank_vars = {month: LpVariable(f"bank_{month}", lowBound=0, cat='Integer') for month in months}
 
 # # Objective Function (maximize total cashflow)
 maxProfit = LpProblem("Rice_Trade_Scheduler", LpMaximize)
-maxProfit += sum(bank_vars[month] for month in months)
+# maxProfit += sum(bank_vars[month] for month in months)
+maxProfit += bank_vars[months[-1]]
 
-rice = ['super_1', 'super_2', 'super_3', 'super_4',
+
+
+rices = ['super_1', 'super_2', 'super_3', 'super_4',
         'kanait_1', 'kanait_2', 'kanait_3', 'kanait_4', 'kanait_5', 'kanait_6',
-        '386_1', '386_2', '386_3']
-op = ['buy', 'sell', 'pay']
+        'cheap_1', 'cheap_2', 'cheap_3']
+ops = ['buy', 'pay', 'sell']
+
 # # Constraints
 for i in range(len(months)):
     if i == 0:
@@ -50,24 +59,31 @@ for i in range(len(months)):
         prev = bank_vars[months[i-1]]
 
     maxProfit += bank_vars[months[i]] == prev + lpSum(
-        [rice_vars_dict[r][o][months[i]] for r in rice for o in op if
-         months[i] in rice_vars_dict[r][o]])
+        [rice_vars_dict[r][op][months[i]] for r in rices for op in ops if
+         months[i] in rice_vars_dict[r][op]])
+
+    maxProfit += prev >= -lpSum(
+        [rice_vars_dict[r][op][months[i]] for r in rices for op in ops[:2] if
+         months[i] in rice_vars_dict[r][op]])
+
+for r,profit in zip(rices, sale_profit):
+    maxProfit += rice_vars_dict[f"price_{r}"] == lpSum(
+            [rice_vars_dict[r][op][month] for op in ops[:2] for month in months if
+             month in rice_vars_dict[r][op]])
+
+
+    maxProfit += lpSum([rice_vars_dict[r]['buy'][month] for month in months if
+                     month in rice_vars_dict[r]['buy']]) == rice_vars_dict[f"price_{r}"]*deposit_perc
+
+    maxProfit += lpSum([rice_vars_dict[r]['sell'][month] for month in months if
+                        month in rice_vars_dict[r]['sell']]) == rice_vars_dict[f"price_{r}"] * profit
 
 
 
+solver = CPLEX_CMD(path='/Applications/CPLEX_Studio221/cplex/bin/x86-64_osx/cplex')
+maxProfit.solve(solver)
 
-
-
-
-
-
-
-# solver = CPLEX_CMD(path='/Applications/CPLEX_Studio221/cplex/bin/x86-64_osx/cplex')
-# max_profit.solve(solver)
-#
-# # %%
-#
-# print("Status:", LpStatus[max_profit.status])
-# for v in max_profit.variables():
-#     if v.varValue > 0:
-#         print(v.name, "=", v.varValue)
+print("Status:", LpStatus[maxProfit.status])
+for v in maxProfit.variables():
+    if v.varValue > 0:
+        print(v.name, "=", v.varValue)
